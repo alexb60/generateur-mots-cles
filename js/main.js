d46b46d2a05d83a65fa3913866c86b1eee6e616f
alexb60 <alexandrebertrand60@yahoo.fr>
2022-03-07 08:33:18 +0100

    modification style

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
+  // MOTS-CLES
   $("form").submit(function (e) {
     e.preventDefault();
+
+    // RECUPERATION DES DONNEES
     let produit = $("#nomProduit").val();
     let marque = $("#marque").val();
     let modele = $("#modele").val();
     let ref = $("#ref").val();
     let spec = $("#spec").val();
 
+    // GENERATEUR
     $("textarea").html(`${produit} ${marque}\n${marque} ${modele}\n${marque} ${ref}\n${produit} ${marque} ${spec}`);
   });
 
